428a40e5ebfaddf5f128325985d3270e23467486
nunerometalero <ornitologob@gmail.com>
2025-05-31 17:50:37 +0200

    Update openai.js

--- utils/openai.js
+++ utils/openai.js
@@ -1,7 +1,7 @@
 const OpenAI = require('openai');
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // Usa dotenv o variable de entorno
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 module.exports = { openai };
